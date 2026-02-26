aab44910b2c067c965443592ff308b67ab3e554d
James Lippitt <711298+jlippitt@users.noreply.github.com>
2026-02-26 08:52:49 +0000

    Implement division

--- utopia-device-n64/Cpu/mul_div.zig
+++ utopia-device-n64/Cpu/mul_div.zig
@@ -7,6 +7,10 @@ pub const MulDivOp = enum {
     MULTU,
     DMULT,
     DMULTU,
+    DIV,
+    DIVU,
+    DDIV,
+    DDIVU,
 };
 
 pub fn mulDiv(comptime op: MulDivOp, core: *Core, word: u32) void {
@@ -51,6 +55,63 @@ pub fn mulDiv(comptime op: MulDivOp, core: *Core, word: u32) void {
             const result: u128 = std.math.mulWide(u64, lhs, rhs);
             core.lo = @truncate(result);
             core.hi = @truncate(result >> 64);
+        },
+        .DIV => {
+            const lhs_i32 = fw.num.truncate(i32, lhs);
+            const rhs_i32 = fw.num.truncate(i32, rhs);
+
+            if (rhs_i32 == 0) {
+                @branchHint(.unlikely);
+                core.lo = if (lhs_i32 < 0) 1 else std.math.maxInt(u64);
+                core.hi = fw.num.signExtend(u64, lhs_i32);
+            } else if (lhs_i32 == std.math.minInt(i32) and rhs_i32 == -1) {
+                @branchHint(.unlikely);
+                core.lo = fw.num.signExtend(u64, @as(i32, std.math.minInt(i32)));
+                core.hi = 0;
+            } else {
+                core.lo = fw.num.signExtend(u64, @divTrunc(lhs_i32, rhs_i32));
+                core.hi = fw.num.signExtend(u64, @rem(lhs_i32, rhs_i32));
+            }
+        },
+        .DIVU => {
+            const lhs_u32: u32 = @truncate(lhs);
+            const rhs_u32: u32 = @truncate(rhs);
+
+            if (rhs_u32 == 0) {
+                @branchHint(.unlikely);
+                core.lo = std.math.maxInt(u64);
+                core.hi = fw.num.signExtend(u64, lhs_u32);
+            } else {
+                core.lo = fw.num.signExtend(u64, lhs_u32 / rhs_u32);
+                core.hi = fw.num.signExtend(u64, lhs_u32 % rhs_u32);
+            }
+        },
+        .DDIV => {
+            const lhs_i64: i64 = @bitCast(lhs);
+            const rhs_i64: i64 = @bitCast(rhs);
+
+            if (rhs_i64 == 0) {
+                @branchHint(.unlikely);
+                core.lo = if (lhs_i64 < 0) 1 else std.math.maxInt(u64);
+                core.hi = @bitCast(lhs_i64);
+            } else if (lhs_i64 == std.math.minInt(i64) and rhs_i64 == -1) {
+                @branchHint(.unlikely);
+                core.lo = @bitCast(@as(i64, std.math.minInt(i64)));
+                core.hi = 0;
+            } else {
+                core.lo = @bitCast(@divTrunc(lhs_i64, rhs_i64));
+                core.hi = @bitCast(@rem(lhs_i64, rhs_i64));
+            }
+        },
+        .DDIVU => {
+            if (rhs == 0) {
+                @branchHint(.unlikely);
+                core.lo = std.math.maxInt(u64);
+                core.hi = lhs;
+            } else {
+                core.lo = lhs / rhs;
+                core.hi = lhs % rhs;
+            }
         },
     }
 
